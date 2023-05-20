a3931d2febb9b57a002b20d1c251a45b31986ef3
Mikhail Pletenchuk <5_mik_5@mail.ru>
2023-05-20 13:49:59 +0500

    creat api service, ending application

--- services/log.services.js
+++ services/log.services.js
@@ -19,3 +19,14 @@ export function printHelp() {
 	`
 	);
 }
+
+export function printWeather(res, icon) {
+	console.log(
+		dedent`${chalk.bgYellow(' WEATHER ')} Погода в городе ${res.name}
+		${icon}  ${res.weather[0].description}
+		Температура: ${res.main.temp} (ощущается как ${res.main.feels_like})
+		Влажность: ${res.main.humidity}%
+		Скорость ветра: ${res.wind.speed}
+	`
+	);
+}
--- weather.js
+++ weather.js
@@ -1,14 +1,50 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printError, printHelp, printSuccess } from './services/log.services.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { getIcon, getWeather } from './services/api.service.js';
+import { printError, printHelp, printSuccess, printWeather } from './services/log.services.js';
+import { getKeyValue, saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+	if (!token.length) {
+		printError('Не передан token');
+		return;
+	}
+
 	try {
-		await saveKeyValue('token', token);
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
 		printSuccess('Токен сохранен');
 	} catch (e) {
 		printError(e.message);
+	}
+};
+
+const saveCity = async (city) => {
+	if (!city.length) {
+		printError('Не передан город');
+		return;
+	}
+
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printSuccess('Город сохранен');
+	} catch (e) {
+		printError(e.message);
+	}
+};
+
+const getForcast = async () => {
+	try {
+		const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city));
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon));
+	} catch (e) {
+		if (e?.response?.status == 404) {
+			printError('Неверно указан город');
+		} else if (e?.response?.status == 401) {
+			printError('Неверно указан токен');
+		} else {
+			printError(e.message);
+		}
 	}
 };
 
@@ -16,11 +52,12 @@ const initCLI = () => {
 	const args = getArgs(process.argv);
 
 	if (args.h) {
-		printHelp();
+		return printHelp();
 	}
 
 	if (args.s) {
 		// Сохранить город
+		return saveCity(args.s);
 	}
 
 	if (args.t) {
@@ -29,6 +66,7 @@ const initCLI = () => {
 	}
 
 	// Вывести погоду
+	return getForcast();
 };
 
 initCLI();
